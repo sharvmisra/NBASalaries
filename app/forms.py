7bb0d9d636ce76b5d432b363eb5a2a9d734078ac
sharvmisra <sharvil.misra@gmail.com>
2024-03-24 21:25:35 -0400

    New commit

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,11 @@
+# app/forms.py
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Optional, URL
 
 class PlayerForm(FlaskForm):
     name = StringField('Player Name', validators=[DataRequired()])
     predicted_salary = StringField('Predicted Salary', validators=[DataRequired()])
     actual_salary = StringField('Actual Salary', validators=[DataRequired()])
+    image_url = StringField('Player Image URL', validators=[Optional(), URL()])
     submit = SubmitField('Add Player')
--- app/routes.py
+++ app/routes.py
@@ -6,25 +6,29 @@ from app.models import Player
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = PlayerForm()
-    if form.validate_on_submit():
-        player = Player(name=form.name.data, predicted_salary=form.predicted_salary.data, actual_salary=form.actual_salary.data)
-        db.session.add(player)
-        db.session.commit()
-        flash('Player added successfully!')
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form)
+    # The form is not actually used on this route in this code
+    return render_template('index.html')
 
 @app.route('/add_player', methods=['GET', 'POST'])
 def add_player():
     form = PlayerForm()
     if form.validate_on_submit():
-        player = Player(name=form.name.data, predicted_salary=form.predicted_salary.data, actual_salary=form.actual_salary.data)
+        print("Form data:", form.data)  # Print form data to the console
+        player = Player(
+            name=form.name.data, 
+            predicted_salary=form.predicted_salary.data, 
+            actual_salary=form.actual_salary.data,
+            image_url=form.image_url.data
+        )
         db.session.add(player)
+        db.session.flush()  # This will allow you to check data before committing
+        print("New player object:", player)  # Print new player object to the console
+        print("New player image URL:", player.image_url)  # Specifically print the image URL
         db.session.commit()
         flash('Player added successfully!')
         return redirect(url_for('index'))
     return render_template('add_player.html', form=form)
+
 
 @app.route('/get_player/<name>')
 def get_player(name):
